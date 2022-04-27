fa890489fd7e19f9ec9ecf891fd0f60723ad20a0
RitzPriti <ritesh_243@yahoo.com>
2022-04-27 21:58:12 +0100

    i have done homework

--- src/Lecture10/PyramidNo.java
+++ src/Lecture10/PyramidNo.java
@@ -6,9 +6,11 @@ public class PyramidNo {
         int a=5;
 
         for(int i=0;i<=a;i++){
-            for(int b=0;b<=i;b++){
-            System.out.print("*");
-        }
+            for(int b=0;b<=i;b++) {
+
+                System.out.print(" *");
+            }
+
             System.out.println();
     }
 }}
--- src/Lecture10/TypeCastingNarrow.java
+++ src/Lecture10/TypeCastingNarrow.java
@@ -4,7 +4,7 @@ public class TypeCastingNarrow {
         public static void main(String[] args){
             float a=10.5345f;
             int b=(int)a;
-            System.out.println("variable a : "+ a);
-            System.out.println("variable b : "+b);
+            System.out.println( a=a);
+            System.out.println( b=b);
         }
 }
